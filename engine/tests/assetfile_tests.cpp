c12e0105d7e9d910ac4b785a273572ab436080e3
abdushukurova <abdushukurova.km@gmail.com>
2025-04-21 15:32:11 +0200

    Updated tests

--- engine/tests/assetfile_tests.cpp
+++ engine/tests/assetfile_tests.cpp
@@ -1,9 +1,8 @@
 #include <sstream>
 #include <iostream>
 #include "enjam/asset.h"
-#include "enjam/assetfile_parser.h"
+#include "enjam/assetfile_reader.h"
 #include "enjam/assetfile_writer.h"
-#include "enjam/type_traits_helpers.h"
 
 int main() {
   using namespace Enjam;
@@ -17,10 +16,18 @@ int main() {
               " array: [ 1, \"str\", 0.42 ],"
               " property_name1     : 0    ";
 
-  std::stringstream input { file };
+
+  struct Input {
+    std::istream& input;
+    std::function<std::unique_ptr<std::istream>(uint64_t)> buffers;
+  };
+
+  std::stringstream strStream { file };
+  Input input { .input = strStream, .buffers = { } };
+  AssetFileReader reader { input };
+
   Asset asset;
-  AssetFileParser parser {input};
-  bool succeed = parser(asset);
+  bool succeed = reader.parse(asset);
   assert(succeed);
 
   assert(asset["float"].as<float_t>() == 3.14f);
@@ -30,30 +37,29 @@ int main() {
   assert(asset["int"].as<int>() == -42);
   assert(asset["uint"].as<unsigned int>() == 42);
 
-  assert(asset["hexadecimal"].is<Asset::int_t>());
-  assert(asset["hexadecimal"].as<size_t>() == 912559);
+  assert(asset["hexadecimal"].is<AssetType::BUFFER>());
 
   assert(asset.at("string"));
-  assert(asset["string"].is<Asset::string_t>());
+  assert(asset["string"].is<AssetType::STRING>());
 
   assert(asset.at("object"));
-  assert(asset.at("object")->is<Asset::object_t>());
+  assert(asset.at("object")->is<AssetType::OBJECT>());
   assert(!asset.at("object")->at("first")->as<std::string>().empty());
 
   assert(asset.at("array"));
-  assert(asset.at("array")->is<Asset::array_t>());
-  assert(asset["array"][0].is<Asset::int_t>());
-  assert(asset["array"][1].is<Asset::string_t>());
-  assert(asset["array"][2].is<Asset::float_t>());
+  assert(asset.at("array")->is<AssetType::ARRAY>());
+  assert(asset["array"][0].is<AssetType::INTEGER>());
+  assert(asset["array"][1].is<AssetType::STRING>());
+  assert(asset["array"][2].is<AssetType::FLOAT>());
 
   auto i = 0;
   for(auto& desc : asset["array"]) {
     if(i == 0) {
-      assert(desc.is<Asset::int_t>());
+      assert(desc.is<AssetType::INTEGER>());
     } else if (i == 1) {
-      assert(desc.is<Asset::string_t>());
+      assert(desc.is<AssetType::STRING>());
     } else if (i == 2) {
-      assert(desc.is<Asset::float_t>());
+      assert(desc.is<AssetType::FLOAT>());
     }
     i++;
   }
@@ -75,9 +81,20 @@ int main() {
   nested2["second_0"] = "we need to go deeper";
   nested2["second_1"] = 0;
 
-  AssetFileSerializer()(newAsset, std::cout);
+  struct Output {
+    std::ostream& out;
+    std::function<std::unique_ptr<std::ostream>(uint64_t)> buffers;
+  };
+
+  Output output {
+    .out = std::cout,
+    .buffers = { }
+  };
+  AssetFileWriter {
+    output,
+    AssetFileWriterFlags::pretty | AssetFileWriterFlags::omitFirstEnclosure
+  }.write(newAsset);
   std::cout << "\n";
-  AssetFileSerializer(AssetFileSerializer::pretty | AssetFileSerializer::omitFirstEnclosure)(newAsset, std::cout);
 
   static_assert(std::is_same_v<AssetIterator<Asset>::reference, Asset&>);
   static_assert(std::is_same_v<AssetIterator<const Asset>::reference, const Asset&>);
